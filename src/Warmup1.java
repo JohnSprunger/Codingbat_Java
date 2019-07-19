012480cafdd138b277bf073fb5a74bffae841770
JohnSprunger <johnleesprunger@gmail.com>
2019-07-19 04:23:32 -0400

    Added icyHot method

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -118,4 +118,9 @@ public class Warmup1 {
 
         return str.substring(0, 2).equals("hi");
     }
+
+    // https://codingbat.com/prob/p192082
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 || temp2 < 0) && (temp1 > 100 || temp2 >100);
+    }
 }
