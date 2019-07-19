38c1d088f395bbb4fe414757e951edf0d5fbde10
JohnSprunger <johnleesprunger@gmail.com>
2019-07-19 04:42:38 -0400

    Added mixStart method

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -152,4 +152,12 @@ public class Warmup1 {
         } else return str;
     }
 
+    // https://codingbat.com/prob/p151713
+    public boolean mixStart(String str) {
+        if(str.length() >= 3)
+            return str.substring(1,3).equals("ix");
+
+        return false;
+    }
+
 }
