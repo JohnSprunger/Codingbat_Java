e49938bcc83428dfc3b7a99e86c20aeb2d9ddedb
JohnSprunger <johnleesprunger@gmail.com>
2019-07-19 04:34:46 -0400

    Added loneTeen method

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -134,4 +134,12 @@ public class Warmup1 {
         return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
     }
 
+    // https://codingbat.com/prob/p165701
+    public boolean loneTeen(int a, int b) {
+        if((a < 13 || a > 19) && (b >= 13 && b <= 19))
+            return true;
+
+        return (b < 13 || b > 19) && (a >= 13 && a <= 19);
+    }
+
 }
