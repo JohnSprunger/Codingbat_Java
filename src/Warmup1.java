75136567f2b49ef44d7a0671938cdcd940f85bc9
JohnSprunger <johnleesprunger@gmail.com>
2019-07-19 03:54:36 -0400

    Added missingChar method

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -58,4 +58,12 @@ public class Warmup1 {
 
         return "not " + str;
     }
+
+    // https://codingbat.com/prob/p190570
+    public String missingChar(String str, int n) {
+        return str.substring(0, n) + str.substring(n + 1);
+    }
+
+    // https://codingbat.com/prob/p123384
+    
 }
