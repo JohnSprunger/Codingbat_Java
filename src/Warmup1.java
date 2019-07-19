21316b20288937585c9404af4e0ba474a5d4f392
JohnSprunger <johnleesprunger@gmail.com>
2019-07-19 03:15:36 -0400

    Added Warmup1-sleepIn

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -1,2 +1,9 @@
+// https://codingbat.com/java/Warmup-1
 public class Warmup1 {
+    // https://codingbat.com/prob/p187868
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return(vacation || !weekday);
+    }
+
+    "Added Warmup1 sleepIn"
 }
--- src/Warmup2.java
+++ src/Warmup2.java
@@ -1,2 +1,5 @@
+
 public class Warmup2 {
+
+
 }
