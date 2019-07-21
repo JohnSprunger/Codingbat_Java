578e3c341bd56ea3bea82e1ad2b08d947374e08e
JohnSprunger <johnleesprunger@gmail.com>
2019-07-20 23:19:53 -0400

    Added countXX method

--- src/Warmup2.java
+++ src/Warmup2.java
@@ -31,5 +31,13 @@ public class Warmup2 {
         return temp;
     }
 
+    // https://codingbat.com/prob/p194667
+    int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length()-1; i++) {
+            if (str.substring(i, i+2).equals("xx")) count++;
+        }
+        return count;
+    }
 
 }
