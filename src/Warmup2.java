b2f7ac7c51bed6e912f735ff421c133e78bbac41
JohnSprunger <johnleesprunger@gmail.com>
2019-07-20 23:22:07 -0400

    Added doubleX method

--- src/Warmup2.java
+++ src/Warmup2.java
@@ -40,4 +40,14 @@ public class Warmup2 {
         return count;
     }
 
+    // https://codingbat.com/prob/p186759
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+
+        if (i == -1) return false;
+        if (i+1 >= str.length()) return false;
+
+        return str.substring(i+1, i+2).equals("x");
+    }
+
 }
